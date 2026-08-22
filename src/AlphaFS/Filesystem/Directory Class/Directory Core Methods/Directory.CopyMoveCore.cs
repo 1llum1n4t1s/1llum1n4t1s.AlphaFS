dd606b4f2e924d8bcd0adb6d036a92b32107a187
IMT <1llum1n4t1@duck.com>
2026-08-23 00:23:19 +0900

    CopySymbolicLinkのルート判定を修正

--- src/AlphaFS/Filesystem/Directory Class/Directory Core Methods/Directory.CopyMoveCore.cs
+++ src/AlphaFS/Filesystem/Directory Class/Directory Core Methods/Directory.CopyMoveCore.cs
@@ -88,7 +88,8 @@ namespace Alphaleonis.Win32.Filesystem
             // フォルダのシンボリックリンクをコピーする。
             // CopyFileEx() では実行できないため、エミュレートする。
 
-            if (File.HasCopySymbolicLink(cma.CopyOptions) && null != fsei && fsei.IsSymbolicLink)
+            if (File.HasCopySymbolicLink(cma.CopyOptions) && isFolder &&
+                File.GetFileSystemEntryInfoCore(cma.Transaction, true, cma.SourcePathLp, false, PathFormat.LongFullPath).IsSymbolicLink)
             {
                var lvi = File.GetLinkTargetInfoCore(cma.Transaction, cma.SourcePathLp, true, PathFormat.LongFullPath);
 
